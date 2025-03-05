847472b63508c56c88e7d9053b3be1e3bb5c0564
pale-isaiah <pale-isaiah@gmx.com>
2025-03-05 18:54:34 +0000

    Add 2.go

---
+++ src/2.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	randomNumber := rand.Intn(10) + 1
+	fmt.Println("Random number:", randomNumber)
+}
